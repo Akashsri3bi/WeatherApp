5feb8ba4fcb18310ce823b1f96b35cb474a9b6ee
Akash srivastava <akashuna3@gmail.com>
2022-01-29 16:21:08 +0530

    changed

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -15,11 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: "Weather"),
+      home: const MyHomePage(title: "WeatherDetails"),
     );
   }
 }
